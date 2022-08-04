6f981f9bf78ca689bd015edc4260264b8a5c011a
Kalen Namiryu <lucas.faehn@outlook.fr>
2022-08-04 17:01:10 +0200

    Add credits commentaries

--- src/main/java/net/kalennamiryu/azuriacore/event/ModEventClientBusEvents.java
+++ src/main/java/net/kalennamiryu/azuriacore/event/ModEventClientBusEvents.java
@@ -6,6 +6,7 @@ import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 
+// This class come from Kaupenjoe 1.18 tutorial under MIT License, link in the CREDITS.txt
 @Mod.EventBusSubscriber(modid = AzuriaCore.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
 public class ModEventClientBusEvents {
 
--- src/main/java/net/kalennamiryu/azuriacore/event/ModEvents.java
+++ src/main/java/net/kalennamiryu/azuriacore/event/ModEvents.java
@@ -3,6 +3,7 @@ package net.kalennamiryu.azuriacore.event;
 import net.kalennamiryu.azuriacore.AzuriaCore;
 import net.minecraftforge.fml.common.Mod;
 
+// This class come from Kaupenjoe 1.18 tutorial under MIT License, link in the CREDITS.txt
 @Mod.EventBusSubscriber(modid = AzuriaCore.MOD_ID)
 public class ModEvents {
 
