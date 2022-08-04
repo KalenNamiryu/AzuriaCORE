5feb7f42daadf06b4330a5670aa5e7debe75110c
Kalen Namiryu <lucas.faehn@outlook.fr>
2022-08-04 16:43:19 +0200

    Prepare events files

---
+++ src/main/java/net/kalennamiryu/azuriacore/event/ModEvents.java
@@ -0,0 +1,11 @@
+package net.kalennamiryu.azuriacore.event;
+
+import net.kalennamiryu.azuriacore.AzuriaCore;
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber(modid = AzuriaCore.MOD_ID)
+public class ModEvents {
+
+    // ADD EVENTS HERE
+
+}
